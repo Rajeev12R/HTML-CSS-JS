b9e9aadeeb2e7c2c997f6db75183e621517620fc
Rajeev Ranjan <135375413+rjranjan2112@users.noreply.github.com>
2023-07-26 18:02:04 +0530

    Javascript

---
+++ 48_Handling_Browser_Event.js
@@ -0,0 +1,16 @@
+
+btn.addEventListener('click', function (e) {
+    alert("Hello World 1 !")
+})
+btn.addEventListener('click', function (e) {
+    alert("Hello World 2 !")
+})
+
+let n = prompt("Who is your favourite person")
+console.log(n);
+
+// HTML Code
+
+<body>
+    <button id="btn">Click Here</button>
+</body>

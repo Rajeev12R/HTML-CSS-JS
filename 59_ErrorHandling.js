72ae879ae987c5a09a37b3ee20938f5020315a75
Rajeev Ranjan <135375413+rjranjan2112@users.noreply.github.com>
2023-07-29 16:07:13 +0530

    Javascript

---
+++ 59_ErrorHandling.js
@@ -0,0 +1,22 @@
+setTimeout(() => {
+    console.log("Hacking Wifi.... Please wait...");
+}, 1000)
+try {
+    console.log(Rajeev);
+}
+catch {
+    console.log("Error 404..But Still Fetching....");
+}
+
+setTimeout(() => {
+    console.log("Fetching Username and Password.... Please wait...");
+}, 2000)
+setTimeout(() => {
+    console.log("Fetching Rajeev's Password.... Please wait...");
+}, 3000)
+setTimeout(() => {
+    console.log("Fetching Password Completed.... Working on it...");
+}, 4000)
+setTimeout(() => {
+    console.log("Password Found i.e Rajeev123xyz...Done!!");
+}, 5000)
